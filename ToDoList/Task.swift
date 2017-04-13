86043ea48786e771c655363d747a3b8d7857619a
user124196 <gregg.newport@gmail.com>
2017-04-12 20:56:22 -0400

    Table View setup

---
+++ ToDoList/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by user124196 on 4/12/17.
+//  Copyright © 2017 Newport Enterprises. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
